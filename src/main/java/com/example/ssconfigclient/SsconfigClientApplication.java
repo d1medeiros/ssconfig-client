1ca659fb745e052e39feb79cc63fe5605ef24665
Diego Medeiros <djbmbb@hotmail.com>
2020-05-02 12:03:17 -0300

    wip

--- src/main/java/com/example/ssconfigclient/SsconfigClientApplication.java
+++ src/main/java/com/example/ssconfigclient/SsconfigClientApplication.java
@@ -1,34 +1,24 @@
 package com.example.ssconfigclient;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SsconfigClientApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SsconfigClientApplication.class, args);
     }
-
 }
 
-@Configuration
-@RequiredArgsConstructor
-class Runner implements CommandLineRunner {
+@RestController("info")
+class Controller {
 
-    @Value("${api.friend}")
-    private String databaseInfo;
-
-    @Override
-    public void run(String... args) throws Exception {
-
-        System.out.println(databaseInfo);
-
-
+    @GetMapping
+    public String info(@Value("${api.friend}") String apiFriend) {
+        return apiFriend;
     }
-}
 
+}
